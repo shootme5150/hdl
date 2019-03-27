e9a0a5561e72e1878619bfb7cefb289a57f5fde7
Istvan Csomortani <istvan.csomortani@analog.com>
2019-03-27 16:28:37 +0000

    axi_hdmi_tx: Update parameter name

--- library/axi_hdmi_tx/axi_hdmi_tx.v
+++ library/axi_hdmi_tx/axi_hdmi_tx.v
@@ -107,7 +107,7 @@ module axi_hdmi_tx #(
   localparam  EMBEDDED_SYNC = (INTERFACE == "16_BIT_EMBEDDED_SYNC") ? 1 : 0;
   localparam  XILINX_7SERIES = 1;
   localparam  XILINX_ULTRASCALE = 2;
-  localparam  ALTERA_5SERIES = 101;
+  localparam  INTEL_5SERIES = 101;
 
   // reset and clocks
 
